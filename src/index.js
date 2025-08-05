6545c23bde68c2eaf3125fe2855a4a9e77be53b8
Mehrab Hasan Sompod <mdmehrabhasansompod@gmail.com>
2025-08-05 12:17:39 +0600

    utils added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
@@ -7,6 +8,46 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("ERROR:",error);
+        throw error
+    })
+
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT} `);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!!", err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import express from "express"
