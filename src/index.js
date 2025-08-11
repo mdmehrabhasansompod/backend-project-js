9522e5f32eb7c249bd838f800421dd7f92118600
Mehrab Hasan Sompod <mdmehrabhasansompod@gmail.com>
2025-08-11 16:31:36 +0600

    completed user logics and fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
+dotenv.config({ path: "./.env" });
 
-
-dotenv.config({
-    path: "./.env"
-})
 
 connectDB()
 .then(()=>{
